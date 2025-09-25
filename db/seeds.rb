a80180de3b97417c987cc48799a9cde51e222c09
Saswat Mishra <saswat.mishra@tamu.edu>
2025-09-24 21:28:05 -0500

    Fixed issue with routes.rb having been setup in app/controllers instead of config

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,22 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-Jul-2010'},
+  {:title => 'The Social Network', :rating => 'PG-13',
+    :release_date => '01-Oct-2010'},
+  {:title => 'La La Land', :rating => 'PG-13',
+    :release_date => '09-Dec-2016'},
+  {:title => 'Dune', :rating => 'PG-13',
+    :release_date => '22-Oct-2021'},
+  {:title => 'Everything Everywhere All at Once', :rating => 'R',
+    :release_date => '25-Mar-2022'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by!(title: movie[:title]) do |m|
+    m.rating = movie[:rating]
+    m.release_date = movie[:release_date]
+  end
 end
